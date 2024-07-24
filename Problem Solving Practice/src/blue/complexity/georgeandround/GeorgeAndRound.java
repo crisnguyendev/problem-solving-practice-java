ac2a73459a149a094f8ba368927c1b7c26473fe9
Vu Nguyen <anhvu2889@gmail.com>
2024-07-24 17:44:16 -0500

    Complete Dynamic Array, Complexity problems

---
+++ Problem Solving Practice/src/blue/complexity/georgeandround/GeorgeAndRound.java
@@ -0,0 +1,38 @@
+package blue.complexity.georgeandround;
+
+import java.util.Scanner;
+
+public class GeorgeAndRound {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int[] a = new int[n];
+        int[] b = new int[m];
+        for (int i = 0; i < n; i++) {
+            a[i] = in.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            b[i] = in.nextInt();
+        }
+        int result = 0;
+        int k = 0;
+        boolean matched = false;
+        for (int i = 0; i < n; i++) {
+            matched = false;
+            while (k < m) {
+                if (b[k] >= a[i]) {
+                    matched = true;
+                    k++;
+                    break;
+                }
+                k++;
+            }
+            if (!matched) {
+                result++;
+            }
+
+        }
+        System.out.println(result);
+    }
+}
---
+++ Problem Solving Practice/src/blue/stackandqueue/streetparade/StreetParade.java
@@ -0,0 +1,42 @@
+package blue.stackandqueue.streetparade;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class StreetParade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            if (n == 0) break;
+            int[] mobiles = new int[n];
+            for (int i = 0; i < n; i++) {
+                mobiles[i] = scanner.nextInt();
+            }
+            tryReorderMobile(mobiles, n);
+        }
+
+    }
+
+    private static void tryReorderMobile(int[] mobiles, int n) {
+        Stack<Integer> sideStreetStack = new Stack<>();
+
+        int nextMobile = 1;
+        for (int i = 0; i < n; i++) {
+            while (!sideStreetStack.isEmpty() && sideStreetStack.peek() == nextMobile) {
+                sideStreetStack.pop();
+                nextMobile++;
+            }
+            if (mobiles[i] == nextMobile) {
+                nextMobile++;
+            } else {
+                sideStreetStack.push(mobiles[i]);
+            }
+        }
+        while (!sideStreetStack.isEmpty() && sideStreetStack.peek() == nextMobile && nextMobile <= n) {
+            sideStreetStack.pop();
+            nextMobile++;
+        }
+        System.out.println(sideStreetStack.isEmpty() ? "yes" : "no");
+    }
+}
