0fc1abae5451a0ac003400a8b96b6a2f168f5222
Vu Nguyen <anhvu2889@gmail.com>
2024-08-27 21:02:05 -0500

    Restructure project

---
+++ Problem Solving Practice/src/BigOCoding/BreadthFirstSearch/FefaAndPark/KefaAndPark.java
@@ -0,0 +1,63 @@
+package BigOCoding.BreadthFirstSearch.FefaAndPark;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class KefaAndPark {
+    private static int n;
+    private static int m;
+    private static int[] cat;
+    private static ArrayList<Integer>[] graph;
+    private static int[] catInPath;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        n = scanner.nextInt();
+        cat = new int[n + 1];
+        catInPath = new int[n + 1];
+        m = scanner.nextInt();
+        graph = new ArrayList[n + 1];
+        for (int i = 1; i <= n; i++) {
+            cat[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            if (graph[x] == null) {
+                graph[x] = new ArrayList<>();
+            }
+            graph[x].add(y);
+        }
+        System.out.println(bfs());
+    }
+
+    private static int bfs() {
+        int result = 0;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(1);
+        catInPath[1] = cat[1];
+        while (!queue.isEmpty()) {
+            int x = queue.poll();
+            boolean isLeaf = isLeaf(x);
+            if (!isLeaf) {
+                if (catInPath[x] > m)
+                    continue;
+                for (int y : graph[x]) {
+                    catInPath[y] = catInPath[x] + cat[y];
+                    queue.add(y);
+                }
+            }
+
+            if (isLeaf && catInPath[x] <= m + 1) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    private static boolean isLeaf(int x) {
+        return graph[x] == null || graph[x].isEmpty();
+    }
+}
---
+++ Problem Solving Practice/src/LeetCode/Easy/BestTimeToBuyAndSellStock/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,22 @@
+package LeetCode.Easy.BestTimeToBuyAndSellStock;
+
+public class BestTimeToBuyAndSellStock {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        BestTimeToBuyAndSellStock solution = new BestTimeToBuyAndSellStock();
+        System.out.println(solution.maxProfit(prices));
+    }
+
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
