424e556a296f3d7e60fbc3429194d82d1b17051b
Vu Nguyen <anhvu2889@gmail.com>
2024-10-18 21:09:41 -0500

    Complete Heap problems

---
+++ Problem Solving Practice/src/BigOCoding/Heap/RestaurantRating/RestaurantRating.java
@@ -0,0 +1,38 @@
+package BigOCoding.Heap.RestaurantRating;
+
+import java.util.*;
+
+public class RestaurantRating {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        PriorityQueue<Integer> topRating = new PriorityQueue<>();
+        PriorityQueue<Integer> nonTopRating = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                return o2 - o1;
+            }
+        });
+        int totalReview = 0;
+        for (int i = 0; i < n; i++) {
+            int action = scanner.nextInt();
+            if (action == 1) {
+                totalReview++;
+                int newRating = scanner.nextInt();
+                if (!topRating.isEmpty()  && topRating.peek() < newRating) {
+                    nonTopRating.add(topRating.poll());
+                    topRating.add(newRating);
+                } else {
+                    nonTopRating.add(newRating);
+                }
+                if(totalReview % 3 == 0)
+                    topRating.add(nonTopRating.poll());
+            } else if (action == 2) {
+                if(topRating.isEmpty())
+                    System.out.println("No reviews yet");
+                else
+                    System.out.println(topRating.peek());
+            }
+        }
+    }
+}
