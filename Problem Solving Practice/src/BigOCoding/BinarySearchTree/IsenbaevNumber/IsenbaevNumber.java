35434f37ae1347d990bbe2b0f4937a95e3600783
Vu Nguyen <anhvu2889@gmail.com>
2024-10-26 22:38:31 -0500

    Complete all Dijkstra problems

--- Problem Solving Practice/src/BigOCoding/BinarySearchTree/IsenbaevNumber/IsenbaevNumber.java
+++ Problem Solving Practice/src/BigOCoding/BinarySearchTree/IsenbaevNumber/IsenbaevNumber.java
@@ -1,68 +1,25 @@
 package BigOCoding.BinarySearchTree.IsenbaevNumber;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class IsenbaevNumber {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int totalTeams = scanner.nextInt();
+        List<List<String>> teamates = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            teamates.add(new ArrayList<>());
+        }
+        for (int i = 0; i < totalTeams; i++) {
+            String member1 = scanner.next();
+            String member2 = scanner.next();
+            String member3 = scanner.next();
+        }
     }
 }
 
-class Node {
-    int data;
-    Node left;
-    Node right;
-
-    public Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-
-    public void add(int value) {
-        if (value < data) {
-            if (left == null) {
-                left = new Node(value);
-            } else {
-                left.add(value);
-            }
-        } else if (value > data) {
-            if (right == null) {
-                right = new Node(value);
-            } else {
-                right.add(value);
-            }
-        }
-    }
-
-    public int countNodes() {
-        int leftNodes = left == null ? 0 : left.countNodes();
-        int rightNodes = right == null ? 0 : right.countNodes();
-        return leftNodes + rightNodes + 1;
-    }
-}
-
-class BinarySearchTree {
-    Node root;
-
-    public BinarySearchTree() {
-        root = null;
-    }
-
-    public void add(int value) {
-        if (root == null) {
-            root = new Node(value);
-        } else {
-            root.add(value);
-        }
-    }
-
-    public int countNodes() {
-        if (root == null) {
-            return 0;
-        } else {
-            return root.countNodes();
-        }
-    }
+class Member{
+    String name;
 }
