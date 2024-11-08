274c3065b4d3cb692227ef1c6c81072f9ca34b32
Vu Nguyen <anhvu2889@gmail.com>
2024-11-07 21:43:50 -0600

    Complete all Trie problems

---
+++ Problem Solving Practice/src/BigOCoding/Trie/ConsistencyChecker/ConsistencyChecker.java
@@ -0,0 +1,55 @@
+package BigOCoding.Trie.ConsistencyChecker;
+
+import java.util.Scanner;
+
+public class ConsistencyChecker {
+    private static boolean isConsistent;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfTestCases = scanner.nextInt();
+        for (int i = 0; i < numberOfTestCases; i++) {
+            Node root = new Node();
+            isConsistent = true;
+            int totalNumbers = scanner.nextInt();
+            for (int j = 0; j < totalNumbers; j++) {
+                String word = scanner.next();
+                if (isConsistent) {
+                    addWord(root, word);
+                }
+            }
+            System.out.printf("Case %d: %s\n", i + 1, isConsistent ? "YES" : "NO");
+        }
+    }
+
+
+    static class Node {
+        Node[] children;
+        int coundWord;
+
+        public Node() {
+            children = new Node[26];
+            coundWord = 0;
+        }
+    }
+
+    private static void addWord(Node root, String word) {
+        Node temp = root;
+        for (int i = 0; i < word.length(); i++) {
+
+            if (temp.children[word.charAt(i) - '0'] == null) {
+                temp.children[word.charAt(i) - '0'] = new Node();
+            } else if (i == word.length() - 1) {
+                isConsistent = false;
+                break;
+            }
+            if (temp.coundWord > 0) {
+                isConsistent = false;
+                break;
+            }
+
+            temp = temp.children[word.charAt(i) - '0'];
+        }
+        temp.coundWord++;
+    }
+}
---
+++ Problem Solving Practice/src/BigOCoding/Trie/DNAPrefix/DNAPrefix.java
@@ -0,0 +1,54 @@
+package BigOCoding.Trie.DNAPrefix;
+
+import java.util.Scanner;
+
+public class DNAPrefix {
+    private static final int ALPHABET_SIZE = 26;
+    private static int maxResult;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numberOfTestCases = scanner.nextInt();
+        for (int i = 0; i < numberOfTestCases; i++) {
+            Trie trie = new Trie();
+            maxResult = 0;
+            int numberOfSamples = scanner.nextInt();
+            for (int j = 0; j < numberOfSamples; j++) {
+                String sample = scanner.next();
+                addWord(trie, sample);
+            }
+            System.out.printf("Case %d: %d\n", i + 1, maxResult);
+        }
+    }
+
+    private static void addWord(Trie trie, String word) {
+        Node temp = trie.root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (temp.children[c - 'A'] == null) {
+                temp.children[c - 'A'] = new Node();
+            }
+            temp = temp.children[c - 'A'];
+            temp.weight++;
+            maxResult = Math.max(maxResult, temp.weight * (i + 1));
+        }
+
+    }
+
+    static class Node {
+        Node[] children;
+        int weight;
+
+        public Node() {
+            children = new Node[ALPHABET_SIZE];
+            weight = 0;
+        }
+    }
+
+    static class Trie {
+        Node root;
+        public Trie() {
+            root = new Node();
+        }
+    }
+}
