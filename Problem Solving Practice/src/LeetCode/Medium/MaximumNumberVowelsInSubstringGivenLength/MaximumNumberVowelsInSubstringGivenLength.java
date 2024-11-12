7d83cef97f61e4535b6f93643cba36087b4d6185
Vu Nguyen <anhvu2889@gmail.com>
2024-11-12 09:53:59 -0600

    Complete Sliding Window problems

---
+++ Problem Solving Practice/src/LeetCode/Medium/MaximumNumberVowelsInSubstringGivenLength/MaximumNumberVowelsInSubstringGivenLength.java
@@ -0,0 +1,31 @@
+package LeetCode.Medium.MaximumNumberVowelsInSubstringGivenLength;
+
+public class MaximumNumberVowelsInSubstringGivenLength {
+    public static void main(String[] args) {
+        String s = "leetcode";
+        int k = 3;
+        System.out.println(new Solution().maxVowels(s, k));
+    }
+}
+
+class Solution {
+    public int maxVowels(String s, int k) {
+        int count = 0;
+        for (int i = 0; i < k; i++) {
+            count += countVowel(s.charAt(i));
+        }
+        int max = count;
+        for (int i = k; i < s.length(); i++) {
+            count += countVowel(s.charAt(i));
+            count -= countVowel(s.charAt(i - k));
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+
+    private int countVowel(char c) {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            return 1;
+        return 0;
+    }
+}
