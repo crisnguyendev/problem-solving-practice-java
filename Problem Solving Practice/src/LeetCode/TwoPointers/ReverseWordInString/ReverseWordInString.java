5979b5175597407bc0e3697609d582c63cb2cb13
Vu Nguyen <anhvu2889@gmail.com>
2024-11-24 09:53:53 -0600

    Complete all Two pointer problems

---
+++ Problem Solving Practice/src/LeetCode/TwoPointers/ReverseWordInString/ReverseWordInString.java
@@ -0,0 +1,40 @@
+package LeetCode.TwoPointers.ReverseWordInString;
+
+public class ReverseWordInString {
+    public static void main(String[] args) {
+        String s = "a good   example";
+        System.out.println(new Solution().reverseWords(s));
+    }
+}
+
+class Solution {
+    private static void reverse(char[] chars, int start, int end) {
+        while (start < end) {
+            char temp = chars[start];
+            chars[start] = chars[end];
+            chars[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static String reverseWords(String sentence) {
+        sentence = sentence.replaceAll("\\s+", " ").trim();
+
+        char[] charArray = sentence.toCharArray();
+        int lastIndex = charArray.length - 1;
+
+        reverse(charArray, 0, lastIndex);
+        int start = 0;
+        for (int end = 0; end <= lastIndex; ++end) {
+            if (end == lastIndex || charArray[end] == ' ') {
+                int endIndex = (end == lastIndex ) ? end : end - 1;
+                reverse(charArray, start, endIndex);
+                start = end + 1;
+            }
+        }
+
+        return new String(charArray);
+    }
+
+}
