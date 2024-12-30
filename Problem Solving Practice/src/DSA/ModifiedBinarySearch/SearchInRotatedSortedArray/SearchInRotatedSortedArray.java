527bd6c33704e18f32867af54235c439b059daa0
Vu Nguyen <anhvu2889@gmail.com>
2024-12-30 10:48:02 -0600

    Complete Modified Binary Search problems 704. Binary Search 278. First Bad Version 528. Random Pick with Weight 33. Search in Rotated Sorted Array

---
+++ Problem Solving Practice/src/DSA/ModifiedBinarySearch/SearchInRotatedSortedArray/SearchInRotatedSortedArray.java
@@ -0,0 +1,36 @@
+package DSA.ModifiedBinarySearch.SearchInRotatedSortedArray;
+
+public class SearchInRotatedSortedArray {
+    public static void main(String[] args) {
+        int[] nums = new int[]{4};
+        int target = 4;
+        System.out.print(new Solution().search(nums, target));
+    }
+}
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (target <= nums[mid] && target >= nums[left]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (target >= nums[mid] && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
