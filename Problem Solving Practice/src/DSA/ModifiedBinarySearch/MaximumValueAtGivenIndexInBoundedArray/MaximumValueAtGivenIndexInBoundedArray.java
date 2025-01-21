381000b269f6b1916099ef5b35fcf3be22d33dd7
Vu Nguyen <anhvu2889@gmail.com>
2025-01-21 09:22:13 +0700

    Complete Modified Binary Search problems

---
+++ Problem Solving Practice/src/DSA/ModifiedBinarySearch/MaximumValueAtGivenIndexInBoundedArray/MaximumValueAtGivenIndexInBoundedArray.java
@@ -0,0 +1,10 @@
+package DSA.ModifiedBinarySearch.MaximumValueAtGivenIndexInBoundedArray;
+
+public class MaximumValueAtGivenIndexInBoundedArray {
+}
+
+class Solution {
+    public int maxValue(int n, int index, int maxSum) {
+        return -1;
+    }
+}
